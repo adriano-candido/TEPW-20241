73d8da9c607a7f053755b13198c2979e6c056a86
Adriano Lima <jink_adriano@hotmail.com>
2024-04-22 20:38:00 -0300

    aula 19-04

--- backend-api/src/main/java/br/edu/unichristus/backend/BackendApiApplication.java
+++ backend-api/src/main/java/br/edu/unichristus/backend/BackendApiApplication.java
@@ -3,7 +3,23 @@ package br.edu.unichristus.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	    info = @Info(
+	        title = "Backend da API construída em Tópicos de Desenv. Web",
+	        version = "1.0",
+	        description = "Trata-se da Documentação da API construída em sala",
+	        contact = @Contact(
+	        	    name = "Adriano Lima",
+	        	    email = "adriano.candido@unichristus.edu.br",
+	        	     url = "https://unichristus.edu.br")
+	    )
+	)
+
 public class BackendApiApplication {
 
 	public static void main(String[] args) {
