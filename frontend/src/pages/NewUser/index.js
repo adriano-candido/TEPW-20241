0efbfee0d573511d7a9fd420ed7c29f907de0fe6
Adriano Lima <jink_adriano@hotmail.com>
2024-06-14 21:55:52 -0300

    aula 14/06

--- frontend/src/pages/NewUser/index.js
+++ frontend/src/pages/NewUser/index.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 import './style.css';
+
+import api from '../../services/api';
 
 import logoImage from '../../assets/logo.png';
 import {FiArrowLeft} from 'react-icons/fi';
 
 export default function NewUser(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    async function createNewUser(e){
+        e.preventDefault();
+
+        const data = {
+            name,
+            email,
+            login,
+            password,
+        }
+
+        try {
+            await api.post('v1/user', data)
+            navigate('/user');
+        } catch (error) {
+            alert('Erro ao cadastrar o usuário');
+        }
+
+    }
+
+
     return (
         <div className="new-user-container">
             <div className="content">
@@ -18,11 +47,19 @@ export default function NewUser(){
                         Home
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="Nome Completo"/>
-                    <input placeholder="E-mail"/>
-                    <input placeholder="Usuário"/>
-                    <input placeholder="Senha"/>
+                <form onSubmit={createNewUser}>
+                    <input placeholder="Nome Completo"
+                    value={name}
+                    onChange={e => setName(e.target.value)}/>
+                    <input placeholder="E-mail"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}/>
+                    <input placeholder="Usuário"
+                    value={login}
+                    onChange={e => setLogin(e.target.value)}/>
+                    <input placeholder="Senha"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}/>
 
                     <button className="button" type="submit">Salvar</button>
                 </form>
